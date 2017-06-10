3f7197f1dd3b1ff68e84977b0d7bc80745cb60a7
Jon Lindell <jhlindell@yahoo.com>
2017-06-10 13:46:21 -0600

    add site navigation panels post-login

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,19 @@
 var $loginBox = $(".loginBox");
 var $signUpBox = $(".signUpBox");
+var $navPanels = $("#navPanels");
 $("#signUpButton").on("click", signUp);
 $("#cancelSignUp").on("click", signUp);
+$("#login").on("click", login);
+$("#plan").on("click", function(){
+    window.location = "/plan.html";
+});
 
 function signUp(event){
   $loginBox.toggle();
   $signUpBox.toggle();
+}
+
+function login(event) {
+  $loginBox.toggle();
+  $navPanels.toggle();
 }
