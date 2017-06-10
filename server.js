dfb87fae2adf655221763a07669a29d0e81968e9
Jon Lindell <jhlindell@yahoo.com>
2017-06-10 12:04:08 -0600

    get basic server running with old website as public assets

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join('public')));
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.listen(port, () => {
+  console.log("listening on port" + port);
+});
