9c51d54fbd05f13f68bf4fc1fac55ecdd1ab7e92
Jon Lindell <jhlindell@yahoo.com>
2017-06-11 13:26:44 -0600

    add migration files for users, categories, stock_items, tags, stock_item_tags. add seed file for users. add users.js route file. chang knex file to properly reference database. server.js updates.

---
+++ migrations/20170611114139_users.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('username', 42).notNullable().defaultTo('');
+    table.string('full_name', 100).notNullable().defaultTo('');
+    table.string('email', 42).notNullable().unique();
+    table.timestamps(true, true);
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.date('birthday').notNullable();
+    table.boolean('isAdmin').defaultTo(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
---
+++ seeds/1_users.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex, Promise) {
+  let data = [{
+    id: 1,
+    username: 'Shotgun',
+    full_name: 'Shannon Rivers',
+    email: 'shannon.rivers@gmail.com',
+    hashed_password: '$2a$06$PeZWh.HgnrcpySYkgyuQ8OpD/kRQKsuEYqI4HsTsJUuSdHjT0vQk2',
+    isAdmin: true,
+    birthday: '10/14/1978'
+  }];
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(() => {
+      return knex('users').insert(data);})
+    .then(() => {
+      return knex.raw("SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))");
+    });
+};
