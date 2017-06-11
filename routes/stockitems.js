693a1ac9a6a7b0db6b3a7e89f1735756c71e21b6
Jon Lindell <jhlindell@yahoo.com>
2017-06-11 15:03:17 -0600

    fixed humps error

--- routes/stockitems.js
+++ routes/stockitems.js
@@ -2,7 +2,6 @@
 
 const knex = require('../knex');
 const express = require('express');
-const humps = require('humps');
 const router = express.Router();
 const queries = require('../queries/si_query');
 
