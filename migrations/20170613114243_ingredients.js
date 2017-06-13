657330b5eaa9607d721c2b554350b4184d43321b
Arlo Shefte-Jacobs <arlo.sj3@gmail.com>
2017-06-13 13:19:18 -0600

    ingredients seed

--- migrations/20170613114243_ingredients.js
+++ migrations/20170613114243_ingredients.js
@@ -4,9 +4,9 @@ exports.up = function(knex, Promise) {
     table.increments('id').primary();
     table.integer('unit').notNullable().references('id').inTable('units');
     table.decimal('measure').notNullable().defaultsTo(0);
-    table.integer('stock_item_id').notNullable().references('id').inTable('stock_items');
+    table.integer('stock_item_id').notNullable().references('si_id').inTable('stock_items');
     table.integer('recipe_id').notNullable().references('id').inTable('recipes');
-  }
+  });
 };
 
 exports.down = function(knex, Promise) {
