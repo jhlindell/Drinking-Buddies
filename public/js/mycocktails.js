941319e0484a6be33535a9b3b1800ff7319056f1
Zach Passarelli <zachary.passarelli@gmail.com>
2017-06-13 10:24:01 -0600

    add more site structure, add js files

---
+++ public/js/mycocktails.js
@@ -0,0 +1,12 @@
+$("#recipe").on("click", function(){
+    window.location = "/recipes.html";
+});
+$("#mybars").on("click", function(){
+    window.location = "/mybars.html";
+});
+$("#stockitems").on("click", function(){
+    window.location = "/stockitems.html";
+});
+$("#mymenus").on("click", function(){
+    window.location = "/mymenus.html";
+});
